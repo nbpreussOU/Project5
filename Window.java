524e89f1a72260f16fcdcd13652b623618438259
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-04-18 20:30:54 -0500

    outlined methods for window class

--- Window.java
+++ Window.java
@@ -1,5 +1,41 @@
+import javax.swing.JFrame;
 
-public class Window
+public class Window extends JFrame
 {
-
+    //need a gridbag
+    //need a label and text area
+    //need a slider
+    //need a button
+    //need a writable text area
+    //need a label and a dropdown menu
+    //need a button
+    //need 5 labels and 5 text areas
+    //need a button and a writable text area
+    
+    //need methods to add a button, text area, writable text area, labels
+    
+    public Window()
+    {
+        
+    }
+    
+    public void addButton()
+    {
+        
+    }
+    
+    public void addTextArea()
+    {
+        
+    }
+    
+    public void addWritableTextArea()
+    {
+        
+    }
+    
+    public void addLabel()
+    {
+        
+    }
 }
