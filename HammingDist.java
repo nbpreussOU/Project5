86ffe7eb295a78f2a31e85ee138d115373429d9a
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-04-19 17:20:46 -0500

    wrote method signatures for all methods needed for the hammingdist

--- HammingDist.java
+++ HammingDist.java
@@ -1,5 +1,49 @@
+import java.util.TreeSet;
 
 public class HammingDist
 {
-
+    public HammingDist()
+    {
+        
+    }
+    
+    public int getMostCommonLetter()
+    {
+        
+    }
+    
+    public int getLeastCommonLetter()
+    {
+        
+    }
+    
+    public String getClosestStation()
+    {
+        
+    }
+    
+    public String getFurthestStation()
+    {
+        
+    }
+    
+    public int[] getHammingDistance()
+    {
+        
+    }
+    
+    public String[] getListOfStationsAtHammingDistance()
+    {
+        
+    }
+    
+    public TreeSet<String> allStations()
+    {
+        
+    }
+    
+    public String[] readInMesonet()
+    {
+        
+    }
 }
