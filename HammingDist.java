49c7f4774105984cf0568615aab027515e6c667a
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-04-19 17:46:55 -0500

    wrote a method to calculate hamming distance

--- HammingDist.java
+++ HammingDist.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.TreeSet;
 
 public class HammingDist
@@ -37,10 +38,61 @@ public class HammingDist
         
     }
     
-    public int[] getHammingDistance()
+    public int[] getHammingDistance(String stID)
     {
+        int temp;
+        int[] arrayHD = new int[5];
+        TreeSet<String> ts = getListStations();
+        //populates arrays with 0 to start
+        Arrays.fill(arrayHD,  0);
         
+        //fills the array with information about the hamming distance
+        for(String s: ts)
+        {
+            //encapsulates the comparison of strings
+            temp = compareStrings(s, stID);
+            
+            //adds one to the correct array number [0] = distance 0, [1] = distance 1, [2] = distance 2, [3] = distance 3, [4] = distance 4
+            switch(temp)
+            {
+                case 0:
+                    arrayHD[0]++;
+                    break;
+                case 1:
+                    arrayHD[1]++;
+                    break;
+                case 2:
+                    arrayHD[2]++;
+                    break;
+                case 3:
+                    arrayHD[3]++;
+                    break;
+                case 4:
+                    arrayHD[4]++;
+                    break;
+            }
+        }
+        return arrayHD;
     }
+    /**
+     * a method for comparing two strings and returning the hamming value
+     * @param station String station name
+     * @param node String station name
+     * @return Hamming Distance for the object
+     */
+    public int compareStrings(String station, String node)
+    {
+        int distance = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            if(station.charAt(i) != node.charAt(i))
+                {
+                    distance++;
+                }
+        }
+        return distance;
+    }
+    
     
     public String[] getListOfStationsAtHammingDistance()
     {
@@ -73,9 +125,11 @@ public class HammingDist
         //finishes the string to be returned
         return stations;
     }
-    
-    public TreeSet<String> addStation(TreeSet<String> stationList, String newStation)
+
+    public TreeSet<String> getListStations()
     {
-        
+        return listStations;
     }
+    
+    
 }
