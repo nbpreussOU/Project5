655e95634ffcdf6e4e13ded23cb9292736413284
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-04-19 17:33:33 -0500

    wrote a method to read in the file

--- HammingDist.java
+++ HammingDist.java
@@ -1,10 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.TreeSet;
 
 public class HammingDist
 {
+    private TreeSet<String> listStations;
     public HammingDist()
     {
-        
+        try
+        {
+            listStations = allStations("Mesonet.txt");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
     
     public int getMostCommonLetter()
@@ -37,12 +47,34 @@ public class HammingDist
         
     }
     
-    public TreeSet<String> allStations()
+    public TreeSet<String> allStations(String filename) throws IOException
     {
+        BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
+        String strg;
+        TreeSet<String> stations = new TreeSet<>();
         
+        //as long as there is another string
+        strg = br.readLine();
+
+        while(strg != null)
+        {
+            //ensures that only station names can be added
+            if(strg.length() == 4)
+            {
+                stations.add(strg);
+            }
+                //reads in the next line, if available
+                strg = br.readLine();
+            
+        }
+        
+        br.close();
+        
+        //finishes the string to be returned
+        return stations;
     }
     
-    public String[] readInMesonet()
+    public TreeSet<String> addStation(TreeSet<String> stationList, String newStation)
     {
         
     }
