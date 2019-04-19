af759c367b8f0e3a29613bc369561e2712715655
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-04-19 17:02:24 -0500

    started laying out JPanels for the right side

--- RightSide.java
+++ RightSide.java
@@ -1,12 +1,41 @@
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class RightSide
 {
     private static JPanel rightSide;
+    private static JPanel row1;
+    private static JPanel row2;
+    private static JPanel row3;
+    private static JPanel row4;
+    private static JPanel row5;
+    
+    private static JLabel explanationOfStatistics;
+    private static JLabel mostCommonLetter;
+    private static JLabel leastCommonLetter;
+    private static JLabel closestStation;
+    private static JLabel furthestStation;
+    
+    private static JTextField mostCommonLetterText;
+    private static JTextField leastCommonLetterText;
+    private static JTextField closestStationText;
+    private static JTextField furthestStationText;
     
     public static JPanel addRightSide()
     {
+        //initialize JPanels
         rightSide = new JPanel();
+        rightSide.setLayout(new BoxLayout(rightSide, BoxLayout.PAGE_AXIS));
+        row1 = new JPanel(new BorderLayout());
+        row2 = new JPanel(new BorderLayout());
+        row3 = new JPanel(new BorderLayout());
+        row4 = new JPanel(new BorderLayout());
+        row5 = new JPanel(new BorderLayout());
+        
         return rightSide;
     }
 }
