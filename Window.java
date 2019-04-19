5d905809502b11211b884b034bb4f2364b95db3a
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-04-18 21:57:00 -0500

    started work on the Jframe and Jpanels to begin to outline the window

--- Window.java
+++ Window.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -21,6 +23,7 @@ public class Window extends JFrame
     //need methods to add a button, text area, writable text area, labels
     private JPanel leftSide;
     private JPanel rightSide;
+    private JFrame jf;
     
     private JButton showStation;
     private JButton calculateHD;
@@ -44,7 +47,20 @@ public class Window extends JFrame
     
     public Window()
     {
+        //initialize JFrame and JPanels, add layout managers
+        jf = new JFrame();
+        jf.setLayout(new BorderLayout());
+        leftSide = new JPanel();
+        leftSide.setLayout(new BorderLayout());
+        rightSide = new JPanel();
+        rightSide.setLayout(new BorderLayout());
         
+        //adds jpanels to the jframe
+        jf.add(leftSide, BorderLayout.WEST);
+        jf.add(rightSide, BorderLayout.EAST);
+        
+        //sizes the JFrame
+        jf.setSize(400,800);
     }
     
     public JButton addButton(String str)
